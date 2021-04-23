6d74edc5cb89fbf244a25b00e9611e58af1b3dc7
measbona <47500564+measbona@users.noreply.github.com>
2021-04-23 22:43:06 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './app.json';
-
-import Home from './src/views/Home';
-import Profile from './src/views/Profile';
-
-AppRegistry.registerComponent(appName, () => Profile);
